a328deb3404b8658c2e26e1398df7bc6cd508cbc
Charlotte Martin <C17502023@mytudublin.ie>
2023-10-02 14:12:02 +0100

    fixed files, readded dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Browse from './components/Browse'
+import Favourites from './components/Favourites'
+import Homepage from './components/Homepage'
+import Profile from './components/Profile'
+import Search from './components/Search'
 
-function App() {
+
+function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome Nerds</h1>
+      <nav>
+        <ul>
+          <li><a href='/'>Home</a></li>
+          <li><a href='/browse'>Browse</a></li>
+          <li><a href='/favourites'>My Favourites</a></li>
+          <li><a href='/profile'>My Profile</a></li>
+          <li><a href='/search'>Search</a></li>
+        </ul>
+      </nav>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/browse' element={<Browse/>}/>
+        <Route path='/favourites'element={<Favourites/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/search' element={<Search/>}/>
+        </Routes>
+      </BrowserRouter>
+
+      
     </div>
   );
 }
