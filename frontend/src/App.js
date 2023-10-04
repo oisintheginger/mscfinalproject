9d4a1d8067d1bc149fd63d25813f1d524fdb5345
fkadir <99030827+fkadir@users.noreply.github.com>
2023-10-04 17:29:20 +0200

    implemented sign up with cognito as a test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,163 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Browse from './components/Browse'
-import Favourites from './components/Favourites'
-import Homepage from './components/Homepage'
-import Profile from './components/Profile'
-import Search from './components/Search'
+import React, { useState } from 'react';
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
-function App(){
-  return (
-    <div className="App">
-      <h1>Welcome Nerds</h1>
-      <nav>
-        <ul>
-          <li><a href='/'>Home</a></li>
-          <li><a href='/browse'>Browse</a></li>
-          <li><a href='/favourites'>My Favourites</a></li>
-          <li><a href='/profile'>My Profile</a></li>
-          <li><a href='/search'>Search</a></li>
-        </ul>
-      </nav>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Homepage/>}/>
-        <Route path='/browse' element={<Browse/>}/>
-        <Route path='/favourites'element={<Favourites/>}/>
-        <Route path='/profile' element={<Profile/>}/>
-        <Route path='/search' element={<Search/>}/>
-        </Routes>
-      </BrowserRouter>
-
-      
-    </div>
-  );
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
 
-export default App;
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+//make this more secure using aws secretsmanager!! 
+// needs to be without client secret because the javascript sdk doesnt support client secret
+const poolData = {
+    UserPoolId: 'eu-north-1_ItPdvWwXq',
+    ClientId: '5eoaskcluu217r636qnef1icib',
+};
+const userPool = new CognitoUserPool(poolData);
+
+export default function App() {
+    const [error, setError] = useState(null);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+            Name: data.get('firstName') + data.get('lastName'),
+        });
+
+        userPool.signUp(
+            data.get('email'),
+            data.get('password'),
+            [
+                { Name: 'name', Value: data.get('firstName') + data.get('lastName') },
+            ],
+            null,
+            (err, result) => {
+                // Unsuccesfull signup
+                if (err) {
+                    setError(err.message);
+                    return;
+                }
+            // Successful signup
+            console.log('Signup result:', result);
+            alert('Signup successful!');
+            }
+        );
+
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="given-name"
+                                    name="firstName"
+                                    required
+                                    fullWidth
+                                    id="firstName"
+                                    label="First Name"
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="lastName"
+                                    label="Last Name"
+                                    name="lastName"
+                                    autoComplete="family-name"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="new-password"
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+                <Copyright sx={{ mt: 5 }} />
+            </Container>
+        </ThemeProvider>
+    );
+}
