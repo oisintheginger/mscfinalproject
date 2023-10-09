aada2f75244f9c1ae286baa0eed540de354e5ab3
Charlotte Martin <C17502023@mytudublin.ie>
2023-10-09 20:13:56 +0100

    Homepage edit

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,8 +1,10 @@
-
+import PropertyCard from "./PropertyCard";
 function Homepage(){
     return(
         <div>
             <h3>Home Page</h3>
+
+            <PropertyCard/>
         </div>
     )
 
