4bde4882d2a1ef59426440304ae7bf8ee373d8fd
fkadir <99030827+fkadir@users.noreply.github.com>
2023-10-18 15:17:40 +0100

    update quickviewproperty
    because of conversion error

--- Backend/src/main/java/msc/HME/properties/QuickViewProperty.java
+++ Backend/src/main/java/msc/HME/properties/QuickViewProperty.java
@@ -1,11 +1,100 @@
 package msc.HME.properties;
 
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class QuickViewProperty {
-    //TO DO: String image
-    public QuickViewProperty() {}
-    public QuickViewProperty(long propertyId, String longitude, String latitude, double price, List<String> tags, String address, String description) {
-    };
 
+    @JsonProperty
+    private long propertyId;
+
+    @JsonProperty
+    private String longitude;
+
+    @JsonProperty
+    private String latitude;
+
+    @JsonProperty
+    private double price;
+
+    @JsonProperty
+    private List<String> tags;
+
+    @JsonProperty
+    private String address;
+
+    @JsonProperty
+    private String description;
+
+    // Default constructor
+    public QuickViewProperty() {}
+
+    // Parameterized constructor
+    public QuickViewProperty(long propertyId, String longitude, String latitude, double price, List<String> tags, String address, String description) {
+        this.propertyId = propertyId;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.price = price;
+        this.tags = tags;
+        this.address = address;
+        this.description = description;
+    }
+
+    // Getters and setters for each field...
+
+    public long getPropertyId() {
+        return propertyId;
+    }
+
+    public void setPropertyId(long propertyId) {
+        this.propertyId = propertyId;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
