17bfdb9d8eae4f2e00ef8dd6f06da8a18fcce12a
fkadir <99030827+fkadir@users.noreply.github.com>
2023-10-31 11:40:11 +0000

    update from main

---
+++ frontend/src/components/Browse.js
@@ -0,0 +1,12 @@
+
+function Browse(){
+    return(
+        <div>
+            <h3>Browse Page</h3>
+        </div>
+    )
+
+}
+
+
+export default Browse;
---
+++ frontend/src/components/Homepage.js
@@ -0,0 +1,12 @@
+
+function Homepage(){
+    return(
+        <div>
+            <h3>Home Page</h3>
+        </div>
+    )
+
+}
+
+
+export default Homepage;
