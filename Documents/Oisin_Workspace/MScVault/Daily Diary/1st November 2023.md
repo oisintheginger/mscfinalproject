fb9012f956704984ba4dbe20375834a5f7db973f
oisintheginger <oisintheginger@gmail.com>
2023-11-02 13:23:54 +0000

    Setup a Test with API Gateway and Amplify
    I was able to set up a test lambda function and a test API gateway. By doing this I was able to understand the process for connecting to the backend. Unfortunately, there is a blocker to continuiing until CORS is enabled on the backend, but once that issue is sorted, it should prove more straightforward to connect to the backend.

---
+++ Documents/Oisin_Workspace/MScVault/Daily Diary/1st November 2023.md
@@ -0,0 +1,8 @@
+
+> [!morning] Morning
+> - Meeting with Sean to create a diagram for the recommendation system
+> - Standup
+
+
+> [!afternoon] Afternoon
+> - Task Here
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,44 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import { Amplify } from "aws-amplify";
+import { Amplify, API } from "aws-amplify";
 import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+Amplify.configure({
+	...awsExports,
+	API: {
+		endpoints: [
+			{
+				name: "OisinTestAPI",
+				endpoint: "https://88u4rq8vn2.execute-api.eu-west-1.amazonaws.com/Dev",
+				service: "lambda",
+				region: "eu-west-1",
+			},
+			{
+				name: "HMEBackend",
+				endpoint: "https://xbqk5zhumf.execute-api.eu-west-1.amazonaws.com/Prod",
+				service: "lambda",
+				region: "eu-west-1",
+			},
+		],
+	},
+});
 
+API.post("OisinTestAPI", "/", {
+	header: {},
+	response: true,
+	body: { message: "This is a test invocation of the test API" },
+})
+	.then((response) => {
+		console.log(response);
+	})
+	.catch((error) => {
+		console.log(error.response);
+	});
+// API.get("HMEBackend", "/api/properties", { headers: {}, response: true })
+// 	.then((response) => console.log(response.data))
+// 	.catch((err) => {
+// 		console.log(err.response);
+// 	});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
