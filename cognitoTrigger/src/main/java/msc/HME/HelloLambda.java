9f83d92d2d6ecaf799bc9238c6345cef5b21b6c3
fkadir <99030827+fkadir@users.noreply.github.com>
2023-11-06 17:01:24 +0000

    clean up?

---
+++ cognitoTrigger/src/main/java/msc/HME/HelloLambda.java
@@ -0,0 +1,11 @@
+package msc.HME;
+
+import java.util.Map;
+
+public class HelloLambda {
+
+    public Object handleRequest(Object requestObject) {
+//        Map requestObjectMap = (Map) requestObject;
+        return requestObject;
+    }
+}
