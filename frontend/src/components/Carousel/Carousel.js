45729030dda6fc899cca0211c9135fb5cc8436e5
Charlotte Martin <105603600+CharlotteMartin1998@users.noreply.github.com>
2023-11-06 16:12:46 +0000

    Adding Carousel Arrows

--- frontend/src/components/Carousel/Carousel.js
+++ frontend/src/components/Carousel/Carousel.js
@@ -2,9 +2,63 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Box, Stack } from "@mui/material";
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 // IMPORT THIS STATEMENT INTO PROPERTY DETAILS PAGE AND IT SHOULD WORK
 // <Carousel propDat={data.imgSrc}/>
+
+// Custom arrow components
+function SampleNextArrow({ style, onClick }) {
+	return (
+	  <div
+		style={{
+		  ...style,
+		  display: 'block',
+		  position: 'absolute',
+		  top: '50%',
+		  right: '25px', 
+		  transform: 'translate(0, -50%)',
+		  fontSize: '30px', 
+		  color: 'white', 
+		  zIndex: 2, 
+		  backgroundColor: 'rgba(0, 0, 0, 0.5)', // Slight background
+		  padding: '10px', 
+		  
+		}}
+		onClick={onClick}
+	  >
+		<ArrowForwardIosIcon style={{ fontSize: 'inherit' }} />
+	  </div>
+	);
+  }
+  
+  function SamplePrevArrow({ style, onClick }) {
+	return (
+	  <div
+		style={{
+		  ...style,
+		  display: 'block',
+		  position: 'absolute',
+		  top: '50%',
+		  left: '25px',
+		  transform: 'translate(0, -50%)',
+		  fontSize: '30px', 
+		  color: 'white', 
+		  zIndex: 2,
+		  backgroundColor: 'rgba(0, 0, 0, 0.5)', // Slight background
+		  padding: '10px', 
+		  
+		}}
+		onClick={onClick}
+	  >
+		<ArrowBackIosNewIcon style={{ fontSize: 'inherit' }} />
+	  </div>
+	);
+  }
+  
+  
+  
 
 const Carousel = ({ propData }) => {
 	const settings = {
@@ -16,6 +70,9 @@ const Carousel = ({ propData }) => {
 		autoplay: false,
 		autoplaySpeed: 2000,
 		centerPadding: 0,
+		prevArrow:<SamplePrevArrow/>,
+		nextArrow:<SampleNextArrow/>,
+		
 	};
 
 	return (
@@ -39,3 +96,4 @@ const Carousel = ({ propData }) => {
 };
 
 export default Carousel;
+
