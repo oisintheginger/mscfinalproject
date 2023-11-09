f8ed0f5cec593d9542452cc3c7db18df1ca7a231
fkadir <99030827+fkadir@users.noreply.github.com>
2023-11-09 16:37:42 +0000

    update properties endpoint

--- Backend/src/main/java/msc/HME/controller/PropertiesController.java
+++ Backend/src/main/java/msc/HME/controller/PropertiesController.java
@@ -1,17 +1,14 @@
 package msc.HME.controller;
 
-import msc.HME.binding.DetailedProperty;
-import msc.HME.binding.JsonPlaceholderService;
-import msc.HME.binding.QuickViewProperty;
+import msc.HME.binding.*;
 import msc.HME.service.PropertyService;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.List;
-
-import org.json.JSONObject;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -27,25 +24,30 @@ public class PropertiesController {
     }
 
     @GetMapping
-    public ResponseEntity<JSONObject> findAll(
+    public ResponseEntity<Object> findAll(
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "50") int size) { // default size hard coded for now
         List<QuickViewProperty> properties = propertyService.getAllQVProperties();
-        ArrayList<Object> paginatedProperties = new ArrayList<>(properties.subList((page-1)*size, (page-1)*size+(size-1)));
-        JSONObject result = new JSONObject();
+        List<QuickViewProperty> paginatedProperties = properties.subList((page-1)*size, (page-1)*size+(size-1));
         // to ensure the total pages is always rounded up this is used: (numerator + denominator - 1) / denominator
-        result.put("totalPages", (properties.size() + size-1)/size);
-        result.put("properties", paginatedProperties);
-        return ResponseEntity.ok(result);
+        int totalPages = (properties.size() + size-1)/size;
+        QVPResponse result = new QVPResponse(totalPages, paginatedProperties);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
-//    @GetMapping("/{id}")
-//    ResponseEntity<QuickViewProperty> findQVProperty(@PathVariable Long id) {
-//        QuickViewProperty property = propertyService.getQVProperty(id);
-//        return ResponseEntity.ok(property);
-//    }
-
     @GetMapping("/{id}")
+    ResponseEntity<Object> findQVProperty(@PathVariable Long id) {
+        try {
+            QuickViewProperty result = propertyService.getQVProperty(id);
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+        } catch (EmptyResultDataAccessException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Property was not found");
+        } catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
+        }
+    }
+
+    @GetMapping("/details/{id}")
     ResponseEntity<DetailedProperty> findPropertyById(@PathVariable Integer id) {
         if (id <= 0) {
             return ResponseEntity.badRequest().build();
@@ -57,21 +59,14 @@ public class PropertiesController {
         return ResponseEntity.ok(property);
     }
 
-
-
-    // get all geolocations + property ids
-
-//    @GetMapping("/{id}")
-//    ResponseEntity<DetailedProperty> findPropertyById(@PathVariable Integer id) {
-////        if (id <= 0) {
-////            return ResponseEntity.badRequest().build();
-////        }
-////        DetailedProperty property = jsonPlaceholderService.loadDetailedProperty(id);
-////        if (property == null) {
-////            return ResponseEntity.notFound().build();
-////        }
-////        return ResponseEntity.ok(property);
-//    }
-
+    @GetMapping("/locations")
+    ResponseEntity<Object> getAll() {
+        try {
+            List<GeoLocation> locations = propertyService.getGeoLocation();
+            return ResponseEntity.ok(locations);
+        } catch (DataAccessException e) {
+           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error accessing the Database");
+        }
+    }
 
 }
