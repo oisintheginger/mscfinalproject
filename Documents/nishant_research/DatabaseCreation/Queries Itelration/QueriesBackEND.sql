f823c55ddb44a7c8a906cd61bd1108fcffe189ad
nickykapur <nickykapur@hotmail.com>
2023-11-09 13:21:59 +0000

    Edit query for property ID

--- Documents/nishant_research/DatabaseCreation/Queries Itelration/QueriesBackEND.sql
+++ Documents/nishant_research/DatabaseCreation/Queries Itelration/QueriesBackEND.sql
@@ -18,30 +18,59 @@ select * from MainInformation mi
 --      ○ Get all quick view properties 
 --      Quick view property has the following fields:§ propertyId§ Geolocation§ Price§ Bathrooms§ Bedrooms§ Highest category scores tags§ streetAddress§ Zipcode§ Overview§ Images
 SELECT 
-    m.propertyID,
+    mi.propertyID,
     a.geoLocation,
-    m.price,
-    m.bathrooms,
-    m.bedrooms,
+    mi.price,
+    mi.bathrooms,
+    mi.bedrooms,
     a.streetAddress,
     a.zipcode,
-    m.overview,
-    i.propertyURL AS Images
+    mi.overview,
+    GROUP_CONCAT(img.propertyURL) AS Images
 FROM 
-    MainInformation m
-JOIN 
-    Addresses a ON m.addressID = a.addressID
+    MainInformation mi
+INNER JOIN 
+    Addresses a ON mi.addressID = a.addressID
 LEFT JOIN 
-    images i ON m.propertyID = i.propertyID
--- WHERE 
---    m.homeStatus = 'available' -- Assuming you want only available properties
+    images img ON mi.propertyID = img.propertyID
+ WHERE 
+     mi.availableNow = 1
 GROUP BY 
-    m.propertyID; -- Group by property ID to avoid duplicate properties in case there are multiple images
+    mi.propertyID;
 
+--  select * from MainInformation
+   select * from user
 
+-- SHOW CREATE TABLE user;
 --      ○ Get property details for specific propertyId 
 --     Property details has the following fields:§ PropertyId§ Price§ Bedrooms§ Bathrooms§ propertyType§ streetAddress§ Zipcode§ Geolocation§ Description§ petsAllowed§ Lastupdated§ Dateposted§ Safetyscore§ servicesScore§ Images 
--- User table 
+SELECT 
+    mi.propertyID,
+    mi.price,
+    mi.bedrooms,
+    mi.bathrooms,
+    mi.homeType AS propertyType,
+    a.streetAddress,
+    a.zipcode,
+    a.geoLocation,
+    mi.overview AS Description,
+    pd.Pets AS petsAllowed,
+    mi.datePosted AS Dateposted,
+    GROUP_CONCAT(img.propertyURL) AS Images
+FROM 
+    MainInformation mi
+INNER JOIN 
+    Addresses a ON mi.addressID = a.addressID
+INNER JOIN 
+    PropertyDetails pd ON mi.propertyDetailsID = pd.propertyDetailsID
+LEFT JOIN 
+    images img ON mi.propertyID = img.propertyID
+WHERE 
+    mi.propertyID = 36428890 -- Replace with the actual property ID
+GROUP BY 
+    mi.propertyID;
+   
+   -- User table 
 --    ○ Get all info for specific user id
 --    ○ Get all favourites for specific user id 
 --    ○ Get all saved searches for specific user id 
