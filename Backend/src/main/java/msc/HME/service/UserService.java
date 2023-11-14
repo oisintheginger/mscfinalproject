3b78c84e52efafc752b6d364292bdc74dd72efd6
fkadir <99030827+fkadir@users.noreply.github.com>
2023-11-14 11:22:37 +0000

    Create UserService.java

---
+++ Backend/src/main/java/msc/HME/service/UserService.java
@@ -0,0 +1,18 @@
+package msc.HME.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public UserService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+}
