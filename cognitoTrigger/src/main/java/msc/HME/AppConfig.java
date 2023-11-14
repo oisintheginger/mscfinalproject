aff846f8e391c34441a628f9875015404b108a00
fkadir <99030827+fkadir@users.noreply.github.com>
2023-11-14 14:34:37 +0000

    update lambda cognito trigger

---
+++ cognitoTrigger/src/main/java/msc/HME/AppConfig.java
@@ -0,0 +1,27 @@
+package msc.HME;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import javax.sql.DataSource;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url("jdbc:mysql://myhmedb.cdc3elu6ufgt.eu-west-1.rds.amazonaws.com/hmedbmain");
+        dataSourceBuilder.username("admin");
+        dataSourceBuilder.password("PassCondria123");
+        return dataSourceBuilder.build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    // Add more bean definitions if needed
+}
+
