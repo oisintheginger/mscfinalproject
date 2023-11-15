7299c30cccea2bfb87a41338258dad1c4c710c2a
Fenna Kadir <99030827+fkadir@users.noreply.github.com>
2023-11-15 10:57:52 +0000

    Revert "User endpoint development "

---
+++ Backend/src/main/java/msc/HME/binding/JsonPlaceholderService.java
@@ -0,0 +1,47 @@
+package msc.HME.binding;
+
+import net.datafaker.Faker;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class JsonPlaceholderService {
+    Faker faker = new Faker(new Locale("ie"));
+
+    public DetailedProperty loadDetailedProperty(int id) {
+        double price = faker.random().nextInt(500, 5000);
+        String longitude = faker.address().longitude();
+        String latitude = faker.address().latitude();
+        int bed = faker.random().nextInt(1, 10);
+        int bath = faker.random().nextInt(1, 10);
+        String[] random = {"apartment", "townhouse", "house", "studio"};
+        String type = random[faker.random().nextInt(0,3)];
+        String address = faker.address().streetAddress();
+        String zipcode = faker.address().zipCode();
+        String[] images = {"https://photos.zillowstatic.com/fp/e3819b051b082ceecf67c8c86e47360f-p_e.jpg",
+        "https://photos.zillowstatic.com/fp/a7568313a2fe28ec2a0df7d516227dac-p_e.jpg",
+                "https://photos.zillowstatic.com/fp/fc2da78a5219a7153981fdb1dd429e8e-p_e.jpg",
+                "https://photos.zillowstatic.com/fp/01bb80ae9214483048c82a3b968377bb-p_e.jpg",
+                "https://photos.zillowstatic.com/fp/a48bfc49573a605a3acb80effb402127-p_e.jpg",
+                "https://photos.zillowstatic.com/fp/cf24caf418ad193c1e1006ccd9f9cf31-p_e.jpg",
+                "https://photos.zillowstatic.com/fp/4fe48247d917a72c5fb2962345db2dc2-p_e.jpg",
+                "https://photos.zillowstatic.com/fp/a5edf4d937f8e37d4a5e6ac736367f5f-p_e.jpg",
+                "https://photos.zillowstatic.com/fp/4992183033f22c8a28c0d6a256b27cfd-p_e.jpg",
+                "https://photos.zillowstatic.com/fp/f1a36cd4265001f7c86fe1ba38585183-p_e.jpg"};
+        String description = faker.lorem().paragraph(15);
+        Date randomPastDate = faker.date().past(365, TimeUnit.DAYS);
+        LocalDate posted = randomPastDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        Date randomDate = faker.date().past(60, TimeUnit.DAYS);
+        LocalDate updated = randomDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        double score = faker.number().randomDouble(1, 0, 10);
+        return new DetailedProperty(id, price, bed, bath, type, address, zipcode, longitude, latitude, description, true, posted, updated, score, images);
+    }
+
+}
--- Backend/src/main/java/msc/HME/controller/PropertiesController.java
+++ Backend/src/main/java/msc/HME/controller/PropertiesController.java
@@ -16,9 +16,11 @@ import java.util.List;
 public class PropertiesController {
 
     private final PropertyService propertyService;
+    private final JsonPlaceholderService jsonPlaceholderService;
 
-    public PropertiesController(PropertyService propertyService) {
-        this.propertyService = propertyService;
+    public PropertiesController(PropertyService qvpService, JsonPlaceholderService jsonPlaceholderService) {
+        this.propertyService = qvpService;
+        this.jsonPlaceholderService = jsonPlaceholderService;
     }
 
     @GetMapping
@@ -45,10 +47,17 @@ public class PropertiesController {
         }
     }
 
-//    @GetMapping("/details/{id}")
-//    ResponseEntity<DetailedProperty> findPropertyById(@PathVariable Integer id) {
-//
-//    }
+    @GetMapping("/details/{id}")
+    ResponseEntity<DetailedProperty> findPropertyById(@PathVariable Integer id) {
+        if (id <= 0) {
+            return ResponseEntity.badRequest().build();
+        }
+        DetailedProperty property = jsonPlaceholderService.loadDetailedProperty(id);
+        if (property == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(property);
+    }
 
     @GetMapping("/locations")
     ResponseEntity<Object> getAll() {
