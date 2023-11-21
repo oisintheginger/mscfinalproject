12238c0a5e93896b330c4b3e1af77dd60aecb630
oisintheginger <oisintheginger@gmail.com>
2023-11-21 16:44:50 +0000

    Rendering Applications

--- frontend/src/Utils/Mutations/ApplicationMutation/ApplicationMutation.js
+++ frontend/src/Utils/Mutations/ApplicationMutation/ApplicationMutation.js
@@ -8,6 +8,40 @@ export function CreateApplicationMutation(
 	const { user } = useAuthenticator((context) => [context.user]);
 
 	return useMutation({
+		mutationFn: (data) => {
+			return API.del("HMEBackend", "/api/user/remove/f", {
+				headers: {
+					Authorization:
+						"Bearer " +
+							user?.getSignInUserSession().getAccessToken().getJwtToken() ||
+						null,
+				},
+				response: true,
+				queryStringParameters: {
+					propertyId: data.propertyId,
+				},
+			});
+		},
+		onMutate: () => {
+			console.log("triggered");
+		},
+		onError: (err) => {
+			errorCallback(err);
+			console.log(err);
+		},
+		onSuccess: (data) => {
+			successCallback(data);
+		},
+	});
+}
+
+export function DeleteApplicationMutation(
+	successCallback = () => {},
+	errorCallback = () => {}
+) {
+	const { user } = useAuthenticator((context) => [context.user]);
+
+	return useMutation({
 		mutationFn: ({ propertyId, message }) => {
 			return API.post("HMEBackend", "/api/user/new/a", {
 				headers: {
