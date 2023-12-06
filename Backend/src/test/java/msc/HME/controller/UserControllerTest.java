c77ddbd7a4a2a30a1c5f5dc06963d04e4960ce61
fkadir <99030827+fkadir@users.noreply.github.com>
2023-12-06 16:05:43 +0000

    Update UserControllerTest.java

--- Backend/src/test/java/msc/HME/controller/UserControllerTest.java
+++ Backend/src/test/java/msc/HME/controller/UserControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
