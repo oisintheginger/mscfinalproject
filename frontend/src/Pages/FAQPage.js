3b8ecc4ec7d5898a1d894c8c858c180f8a0bd17f
oisintheginger <oisintheginger@gmail.com>
2023-12-07 10:07:23 +0000

    Update FAQPage.js

--- frontend/src/Pages/FAQPage.js
+++ frontend/src/Pages/FAQPage.js
@@ -19,7 +19,7 @@ function FAQPage() {
 					</Box>
 					<Box mt={5}>
 						<Typography variant="h2">
-							Information about Scoring System{" "}
+							Information about Scoring System
 						</Typography>
 						<Divider />
 						<Typography variant="body2">
@@ -46,11 +46,19 @@ function FAQPage() {
 					</Box>
 					<Box mt={5}>
 						<Typography variant="h2">
-							Information about Recommendation System{" "}
+							Information about Recommendation System
 						</Typography>
 						<Divider />
 						<Typography variant="body2">
-							NISHANTY GIMME THE DETAILS BUT STOOPIDIFIED PLEEEEEAAAAASE!!!!!!
+							With the properties you 'Favorite,' you provide us with valuable
+							feedback about your preferences in homes or apartments. This
+							feedback is instrumental in helping our recommendation system
+							comprehend your unique taste in properties, enabling us to suggest
+							the most suitable options for you. As you continue to 'Favorite'
+							more properties, our recommender system accumulates a wealth of
+							information, allowing us to make increasingly precise
+							recommendations for your ideal home or apartment. Your input is
+							invaluable in enhancing your property search experience.
 						</Typography>
 					</Box>
 				</Paper>
